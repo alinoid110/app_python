08bf85048f1f2086181ff5d286a2984862dc7e90
alinoid110 <alinoid110@mail.ru>
2025-04-02 11:33:52 +0500

    add test

--- VK_API_Check/VK_API_Check/settings.py
+++ VK_API_Check/VK_API_Check/settings.py
@@ -27,7 +27,8 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-&2^_gufd14p-)tg1ac!)o1mi-)7l*hdl1#=8_b@&af7g2!d-t^'
-SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
